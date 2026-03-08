9dc103f130fe928f9d85b2aa1cf1df20f990a66f
DmytroShishkin NC <137729194+DmytroShishkin@users.noreply.github.com>
2026-03-08 13:07:48 +0100

    Templete Page Response

--- src/main/java/com/dmytro/language_learning_api/service/TranslationService.java
+++ src/main/java/com/dmytro/language_learning_api/service/TranslationService.java
@@ -1,15 +1,14 @@
 package com.dmytro.language_learning_api.service;
 
 import com.dmytro.language_learning_api.dto.TranslationDTO;
-import com.dmytro.language_learning_api.dto.response.TranslationRespons;
+import com.dmytro.language_learning_api.dto.response.PageResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface TranslationService {
     //List<TranslationDTO> getTranslationsByWordId(@PathVariable UUID wordId, int pageNo, int pageSize);
-    TranslationRespons getTranslationsByWordId(@PathVariable UUID wordId, int pageNo, int pageSize);
+    PageResponse<TranslationDTO> getTranslationsByWordId(@PathVariable UUID wordId, int pageNo, int pageSize);
     TranslationDTO addTranslation(UUID wordId, TranslationDTO translationDto);
     TranslationDTO getTranslationById(UUID translationId);
     TranslationDTO updateTranslation(UUID translationId, TranslationDTO translationDto);
--- src/main/java/com/dmytro/language_learning_api/service/WordsService.java
+++ src/main/java/com/dmytro/language_learning_api/service/WordsService.java
@@ -3,9 +3,8 @@ package com.dmytro.language_learning_api.service;
 import com.dmytro.language_learning_api.dto.TranslationDTO;
 import com.dmytro.language_learning_api.dto.UpdateWordRequest;
 import com.dmytro.language_learning_api.dto.WordsDTO;
-import com.dmytro.language_learning_api.dto.response.WordRespons;
+import com.dmytro.language_learning_api.dto.response.PageResponse;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface WordsService {
@@ -14,7 +13,7 @@ public interface WordsService {
     WordsDTO getWordById(UUID wordId);
     //List<WordsDTO> getWordsByUser(UUID ownerId);
     WordsDTO updateWord(UUID wordId, UpdateWordRequest updateWordRequest);
-    WordRespons getAllWordsByOwnerId(UUID wordId, int pageNo, int pageSize);
+    PageResponse<WordsDTO> getAllWordsByOwnerId(UUID wordId, int pageNo, int pageSize);
     WordsDTO addTranslationToWord(UUID wordId, TranslationDTO dto);
     void addSynonym(UUID wordId, UUID synonymId);
     void deleteWord(UUID wordId);
